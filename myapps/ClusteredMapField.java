0cb4ead71a5d12e4ac9e87362421a4778899809d
Harish Rathi <hrathi@rim.com>
2012-07-08 10:55:15 -0400

    formatting and code comments

--- myapps/ClusteredMapField.java
+++ myapps/ClusteredMapField.java
@@ -16,113 +16,112 @@ import net.rim.device.api.ui.Color;
 public class ClusteredMapField extends MapField {
 	Vector _gsFolder = new Vector();
 	StyleSet _styles;
-	
+
 	public ClusteredMapField() {
 		super();
 		_styles = getDrawingStyles();
 		setDimensions(new MapDimensions(Display.getWidth(), Display.getHeight()));
 	}
-	
+
+	// crux of the algorithm to display one icon for a cluster.
+	// it works as follows: for a given mappable item (GsImage),
+	// if its rectangular bounding box intersects with another "Cluster"
+	// then add it to that cluster. If it doesn't intersect with any other
+	// cluster, create a new cluster and mark this item as "Lead" of the
+	// new cluster.
 	public void updateGsFolder(GsImage a) {
-		MapDimensions currentDimensions = getDimensions();
-		
 		// TODO: we should really update mappables in the current dimensions
-		//if( a.getBoundingBox().intersects(currentDimensions.getBoundingBox())) {
-			getModel().add(a, "all", false);
-			setOuterBox(a);
-			
-			for( Enumeration e = _gsFolder.elements(); e.hasMoreElements();) {
-				Object o = e.nextElement();
-				if( o instanceof ClusterMarkers ) {
-					ClusterMarkers cluster = (ClusterMarkers)o;
-					LatLonRectangle c = cluster.getBoundingBox();
-					if( c.intersects(a.getBoundingBox()) ) {
-						cluster.addElement(a);
-						return;
-					}
+		// if(a.getBoundingBox().intersects(currentDimensions.getBoundingBox())) {
+		//		MapDimensions currentDimensions = getDimensions();
+		
+		getModel().add(a, "all", false);
+		setOuterBox(a);
+
+		for (Enumeration e = _gsFolder.elements(); e.hasMoreElements();) {
+			Object o = e.nextElement();
+			if (o instanceof ClusterMarkers) {
+				ClusterMarkers cluster = (ClusterMarkers) o;
+				LatLonRectangle c = cluster.getBoundingBox();
+				if (c.intersects(a.getBoundingBox())) {
+					cluster.addElement(a);
+					return;
 				}
 			}
-			
-			ClusterMarkers cm = new ClusterMarkers(a.getLat(), a.getLon());
-			cm.addElement(a);
-			getModel().add(cm, "lead", true);
-			_gsFolder.addElement(cm);
-		//}
+		}
+
+		ClusterMarkers cm = new ClusterMarkers(a.getLat(), a.getLon());
+		cm.addElement(a);
+		getModel().add(cm, "lead", true);
+		_gsFolder.addElement(cm);
 	}
-	
+
 	private void setOuterBox(GsImage g) {
 		LatLonRectangle gRect = g.getBoundingBox();
-		double expandedDim = (getDimensions().getBoundingBox().getWidth()/100000.0)/20.0; 
+		double expandedDim = (getDimensions().getBoundingBox().getWidth() / 100000.0) / 20.0;
 		gRect.setLeft(g.getLon() - expandedDim);
 		gRect.setBottom(g.getLat() - expandedDim);
 		gRect.setRight(g.getLon() + expandedDim);
 		gRect.setTop(g.getLat() + expandedDim);
-		
-		/*
-		MapPoint[] pts = new MapPoint[5];
-		pts[0] = new MapPoint(gRect.getTop(), gRect.getLeft());
-		pts[1] = new MapPoint(gRect.getTop(), gRect.getRight());
-		pts[2] = new MapPoint(gRect.getBottom(), gRect.getRight());
-		pts[3] = new MapPoint(gRect.getBottom(), gRect.getLeft());
-		pts[4] = new MapPoint(gRect.getTop(), gRect.getLeft());
 
-		MapPolyLine line = new MapPolyLine(pts) {
-			public boolean isClusterable() {
-				return false;
-			}
-		};
-		
-		getModel().add(line, "line", true);
-		*/
+		/*
+		 * MapPoint[] pts = new MapPoint[5]; pts[0] = new
+		 * MapPoint(gRect.getTop(), gRect.getLeft()); pts[1] = new
+		 * MapPoint(gRect.getTop(), gRect.getRight()); pts[2] = new
+		 * MapPoint(gRect.getBottom(), gRect.getRight()); pts[3] = new
+		 * MapPoint(gRect.getBottom(), gRect.getLeft()); pts[4] = new
+		 * MapPoint(gRect.getTop(), gRect.getLeft());
+		 * 
+		 * MapPolyLine line = new MapPolyLine(pts) { public boolean
+		 * isClusterable() { return false; } };
+		 * 
+		 * getModel().add(line, "line", true);
+		 */
 	}
 
 	public void eventOccurred(MappableChangeEvent arg0) {
 		// TODO Auto-generated method stub
-		return;	
+		return;
 	}
 
 	public void resetPoints() {
 		_gsFolder = new Vector();
 		getModel().removeAll();
-	}	
-	
+	}
+
 	public void setStyle() {
 		Style classStyle = new Style();
-		classStyle.setLabelFillColor( Color.BLACK );
-		classStyle.setLabelFontColor( Color.WHITE );
+		classStyle.setLabelFillColor(Color.BLACK);
+		classStyle.setLabelFontColor(Color.WHITE);
 		classStyle.setEdgeColor(Color.RED);
 		classStyle.setEdgeSize(3);
-		_styles.addClassBasedStyle( ClusterMarkers.class, classStyle );
+		_styles.addClassBasedStyle(ClusterMarkers.class, classStyle);
 	}
-	
+
 	public int getPreferredHeight() {
-		if( getScreen() != null ) {
+		if (getScreen() != null) {
 			return getScreen().getVisibleHeight();
 		}
 		return super.getPreferredHeight();
-    }
+	}
 
-    public int getPreferredWidth() {
-    	if( getScreen() != null ) {
-    		return getScreen().getVisibleWidth();
-    	}
-    	return super.getPreferredWidth();
-    }
-    
+	public int getPreferredWidth() {
+		if (getScreen() != null) {
+			return getScreen().getVisibleWidth();
+		}
+		return super.getPreferredWidth();
+	}
+
 	public void updateClusterCounters() {
-		for(int i = _gsFolder.size() - 1; i >= 0 ; i--) {
-			ClusterMarkers cm = (ClusterMarkers)_gsFolder.elementAt(i);
+		for (int i = _gsFolder.size() - 1; i >= 0; i--) {
+			ClusterMarkers cm = (ClusterMarkers) _gsFolder.elementAt(i);
 			int clusterSize = cm.getSize();
-			if( clusterSize >= ClusterMarkers.SUPER_CLUSTER_MIN_VALUE ) {
+			if (clusterSize >= ClusterMarkers.SUPER_CLUSTER_MIN_VALUE) {
 				cm.setIconUri("res://img/zoom.png");
 				cm.setThumbnail(PicturesMapScreen.getScaledBitmapImage(cm.getFirstThumbnailUri(), 8, 60, 60));
 				cm.setName(cm.getName());
-			}
-			else {
+			} else {
 				cm.setIconUri("res://img/red/number_" + clusterSize + ".png");
 			}
 		}
 	}
 }
-
-
--- myapps/ProgressIndicatorScreen.java
+++ myapps/ProgressIndicatorScreen.java
@@ -29,230 +29,214 @@ import com.drew.imaging.jpeg.JpegProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.wrapper.File;
 
-class ProgressIndicatorScreen extends PopupScreen
-{    
-    ProgressIndicatorView view = new ProgressIndicatorView(0);
-    ProgressIndicatorModel model = new ProgressIndicatorModel(1, 100, 1);
-    ProgressIndicatorController controller = new ProgressIndicatorController();        
+class ProgressIndicatorScreen extends PopupScreen {
+	ProgressIndicatorView view = new ProgressIndicatorView(0);
+	ProgressIndicatorModel model = new ProgressIndicatorModel(1, 100, 1);
+	ProgressIndicatorController controller = new ProgressIndicatorController();
 
-    ProgressThread _progressThread;
+	ProgressThread _progressThread;
 	private Vector _jpegFileNames;
 	private SimpleSortingVector _imgVector;
 	private PicturesMapScreen _mapScreen;
 	private PersistentObject _store;
 	private Hashtable _coordinates;
 	private LabelField _geoCodedImageCount;
-	
-    static final long PIC_NAMES_PERSISTENT_STORE_ID = 0xf2cc90250b17fbdfL;
 
+	static final long PIC_NAMES_PERSISTENT_STORE_ID = 0xf2cc90250b17fbdfL;
 
-    public ProgressIndicatorScreen(PicturesMapScreen mapScreen)
-    {  
-    	super(new VerticalFieldManager());
-    	_mapScreen = mapScreen;
+	public ProgressIndicatorScreen(PicturesMapScreen mapScreen) {
+		super(new VerticalFieldManager());
+		_mapScreen = mapScreen;
 
-    	_imgVector = new SimpleSortingVector();
-    	_geoCodedImageCount = new LabelField();
-    	
-        
-        model.setController(controller);
-        view.setModel(model);
-        view.setController(controller);        
-        controller.setModel(model);
-        controller.setView(view);
-        
-        view.setLabel("Retrieving images...");
-        view.createProgressBar(Field.FIELD_HCENTER);
-        
-        HorizontalFieldManager hm = new HorizontalFieldManager();
-        hm.add(new LabelField("Images with GPS information:"));
-        hm.add(_geoCodedImageCount);
+		_imgVector = new SimpleSortingVector();
+		_geoCodedImageCount = new LabelField();
 
-        view.add(hm);
-        add(view);
-        
-        // Retrieve the persistent object for this application 
-        _store = PersistentStore.getPersistentObject(PIC_NAMES_PERSISTENT_STORE_ID);                                    
-        
-        synchronized(_store)
-        {         
-            // If the PersistentObject is empty, initialize it
-            if(_store.getContents() == null)
-            {
-                _store.setContents(new Hashtable());  
-            }      
-        }
-        
-        // Retrieve the saved Meeting objects from the persistent store
-        _coordinates = (Hashtable)_store.getContents(); 
-        
-        _progressThread = new ProgressThread();
-        _progressThread.start();
-    } 
-    
-    
-    public void done() {
-    	_mapScreen.setImageVector(_imgVector);
-    	persist();
-    	
-    	UiApplication.getUiApplication().invokeLater(new Runnable() {
-            public void run () {
-            	ProgressIndicatorScreen.this.close();
-            }
-        });    	
-    }
-    
-    public void persist()
-    {
-        // Synchronize on the PersistentObject so that no other object can
-        // acquire the lock before we finish the commit operation.
-        synchronized( _store )
-        { 
-            _store.setContents(_coordinates);
-            PersistentObject.commit(_store);
-        }
-    } 
+		model.setController(controller);
+		view.setModel(model);
+		view.setController(controller);
+		controller.setModel(model);
+		controller.setView(view);
 
-    
-    // A thread that simulates the processing of data
-    class ProgressThread extends Thread
-    {
-        public void run()
-        {
-        	_jpegFileNames = getPictureFileNames();
-        	int numFiles = _jpegFileNames.size();
+		view.setLabel("Retrieving images...");
+		view.createProgressBar(Field.FIELD_HCENTER);
 
-        	ProgressIndicatorScreen.this.model.setValueMax(numFiles+1);
-			for(int i = 0; i < numFiles; ++i)
-            {           
+		HorizontalFieldManager hm = new HorizontalFieldManager();
+		hm.add(new LabelField("Images with GPS information:"));
+		hm.add(_geoCodedImageCount);
+
+		view.add(hm);
+		add(view);
+
+		// Retrieve the persistent object for this application
+		_store = PersistentStore.getPersistentObject(PIC_NAMES_PERSISTENT_STORE_ID);
+
+		synchronized (_store) {
+			// If the PersistentObject is empty, initialize it
+			if (_store.getContents() == null) {
+				_store.setContents(new Hashtable());
+			}
+		}
+
+		// Retrieve the saved Meeting objects from the persistent store
+		_coordinates = (Hashtable) _store.getContents();
+
+		_progressThread = new ProgressThread();
+		_progressThread.start();
+	}
+
+	public void done() {
+		_mapScreen.setImageVector(_imgVector);
+		persist();
+
+		UiApplication.getUiApplication().invokeLater(new Runnable() {
+			public void run() {
+				ProgressIndicatorScreen.this.close();
+			}
+		});
+	}
+
+	public void persist() {
+		// Synchronize on the PersistentObject so that no other object can
+		// acquire the lock before we finish the commit operation.
+		synchronized (_store) {
+			_store.setContents(_coordinates);
+			PersistentObject.commit(_store);
+		}
+	}
+
+	// A thread that simulates the processing of data
+	class ProgressThread extends Thread {
+		public void run() {
+			_jpegFileNames = getPictureFileNames();
+			int numFiles = _jpegFileNames.size();
+
+			ProgressIndicatorScreen.this.model.setValueMax(numFiles + 1);
+			for (int i = 0; i < numFiles; ++i) {
 				String fileName = (String) _jpegFileNames.elementAt(i);
-				
-				if( _coordinates.containsKey(fileName) ) {
+
+				if (_coordinates.containsKey(fileName)) {
 					Object newCoord = _coordinates.get(fileName);
-					if( newCoord instanceof PersitableCoordinates ) {
-						GeoImage gsImg = new GeoImage(fileName, ((PersitableCoordinates)newCoord).getLat(), ((PersitableCoordinates)newCoord).getLon(), fileName);
-						_imgVector.addElement(gsImg);	
+					if (newCoord instanceof PersitableCoordinates) {
+						GeoImage gsImg = new GeoImage(fileName, ((PersitableCoordinates) newCoord).getLat(),
+								((PersitableCoordinates) newCoord).getLon(), fileName);
+						_imgVector.addElement(gsImg);
 					}
 				} else {
 					Coordinates newCoord = getJPEGCoordinates(fileName);
-					if( newCoord instanceof Coordinates ) {
-						GeoImage gsImg = new GeoImage(fileName, newCoord.getLatitude(), newCoord.getLongitude(), fileName);
-						_imgVector.addElement(gsImg);	
-						_coordinates.put(fileName, new PersitableCoordinates(newCoord.getLatitude(), newCoord.getLongitude()));
+					if (newCoord instanceof Coordinates) {
+						GeoImage gsImg = new GeoImage(fileName, newCoord.getLatitude(), newCoord.getLongitude(),
+								fileName);
+						_imgVector.addElement(gsImg);
+						_coordinates.put(fileName,
+								new PersitableCoordinates(newCoord.getLatitude(), newCoord.getLongitude()));
 					} else {
 						_coordinates.put(fileName, "");
-					}					
+					}
 				}
-                ProgressIndicatorScreen.this.model.setValue(i+1);
-                
-                if( i % 10 == 0 || i < 10 ) {
-	            	UiApplication.getUiApplication().invokeLater(new Runnable() {
-	                    public void run () {
-	                    	_geoCodedImageCount.setText(""+_imgVector.size());
-	                    }
-	                });    	
-                }
+				ProgressIndicatorScreen.this.model.setValue(i + 1);
 
-            } 
-			
+				if (i % 10 == 0 || i < 10) {
+					UiApplication.getUiApplication().invokeLater(new Runnable() {
+						public void run() {
+							_geoCodedImageCount.setText("" + _imgVector.size());
+						}
+					});
+				}
+
+			}
+
 			done();
-			
-        }
-        
-        
-        
-    	private Vector getPictureFileNames() {
-    		FileConnection fc = null;
-    		String root = null;
-    		Vector jpegFileNames = new Vector();
 
-    		Enumeration e = FileSystemRegistry.listRoots();
-    		while (e.hasMoreElements()) {
-    			root = (String) e.nextElement();
-    			String prefixPath = null;
-    			if (root.equalsIgnoreCase("sdcard/")) {
-    				try {
-    					prefixPath = "file:///SDCard/BlackBerry/camera/";
-    					fc = (FileConnection) Connector.open(prefixPath,
-    							Connector.READ);
-    					if (fc != null) {
-    						addFileNames(jpegFileNames, prefixPath, fc.list());
-    					}
-    				} catch (Exception ioex) {
-    					Dialog.alert(ioex.toString());
-    				}
-    			}
-    			else if( root.equalsIgnoreCase("store/") ) 
-        		{
-    				try 
-    				{ 
-              		 	prefixPath = "file:///store/home/user/pictures/";
-    					fc = (FileConnection) Connector.open(prefixPath, Connector.READ);
-    					if (fc != null) {
-    						addFileNames(jpegFileNames, prefixPath, fc.list());
-    					}
-    				} catch (Exception ioex) {
-                	   Dialog.alert(ioex.toString());
-    				}
-        		}
-    		}
-    		
-    		return jpegFileNames;
-    	}
+		}
 
-    	private void addFileNames(Vector names, String prefixPath, Enumeration e) {
-    		while (e.hasMoreElements()) {
-    			String s = (String) e.nextElement();	
+		private Vector getPictureFileNames() {
+			FileConnection fc = null;
+			String root = null;
+			Vector jpegFileNames = new Vector();
 
-    			if (s.toLowerCase().endsWith("jpg")) {
-    				names.addElement(prefixPath + s);
-    			}
-    		}
-    	}
-    	
-    	private Coordinates getJPEGCoordinates(String fileName) {
-    		File jpegFile = new File(fileName);
-    		Metadata metaData = new Metadata();
-    		try {
-    			metaData = JpegMetadataReader.readMetadata(jpegFile);
-    		}catch (JpegProcessingException je) {
-    			System.err.println("error 1a");
-    			//close();
-    		}
+			Enumeration e = FileSystemRegistry.listRoots();
+			while (e.hasMoreElements()) {
+				root = (String) e.nextElement();
+				String prefixPath = null;
+				if (root.equalsIgnoreCase("sdcard/")) {
+					try {
+						prefixPath = "file:///SDCard/BlackBerry/camera/";
+						fc = (FileConnection) Connector.open(prefixPath, Connector.READ);
+						if (fc != null) {
+							addFileNames(jpegFileNames, prefixPath, fc.list());
+						}
+					} catch (Exception ioex) {
+						Dialog.alert(ioex.toString());
+					}
+				} else if (root.equalsIgnoreCase("store/")) {
+					try {
+						prefixPath = "file:///store/home/user/pictures/";
+						fc = (FileConnection) Connector.open(prefixPath, Connector.READ);
+						if (fc != null) {
+							addFileNames(jpegFileNames, prefixPath, fc.list());
+						}
+					} catch (Exception ioex) {
+						Dialog.alert(ioex.toString());
+					}
+				}
+			}
 
-    		String strLat = metaData.getLatitude();
-    		String strLon = metaData.getLongitude();
+			return jpegFileNames;
+		}
 
-    		if( strLat != null && strLon != null ) {
-    			double lat = Double.parseDouble(strLat);
-    			double lon = Double.parseDouble(strLon);
-    			
-    			jpegFile.setLat(lat);
-    			jpegFile.setLon(lon);
-    			return new Coordinates(jpegFile.getLat(), jpegFile.getLon(), Float.NaN);
-    		}
-    		
-    		return null;
-    		
-        }
+		private void addFileNames(Vector names, String prefixPath, Enumeration e) {
+			while (e.hasMoreElements()) {
+				String s = (String) e.nextElement();
 
-    }   
+				if (s.toLowerCase().endsWith("jpg")) {
+					names.addElement(prefixPath + s);
+				}
+			}
+		}
+
+		private Coordinates getJPEGCoordinates(String fileName) {
+			File jpegFile = new File(fileName);
+			Metadata metaData = new Metadata();
+			try {
+				metaData = JpegMetadataReader.readMetadata(jpegFile);
+			} catch (JpegProcessingException je) {
+				System.err.println("error 1a");
+				// close();
+			}
+
+			String strLat = metaData.getLatitude();
+			String strLon = metaData.getLongitude();
+
+			if (strLat != null && strLon != null) {
+				double lat = Double.parseDouble(strLat);
+				double lon = Double.parseDouble(strLon);
+
+				jpegFile.setLat(lat);
+				jpegFile.setLon(lon);
+				return new Coordinates(jpegFile.getLat(), jpegFile.getLon(), Float.NaN);
+			}
+
+			return null;
+
+		}
+
+	}
 }
 
 class PersitableCoordinates implements Persistable {
 
 	private double _lat;
 	private double _lon;
-	public PersitableCoordinates(double lat, double lon)
-	{
+
+	public PersitableCoordinates(double lat, double lon) {
 		_lat = lat;
 		_lon = lon;
 	}
-	
+
 	public double getLat() {
 		return _lat;
 	}
-	
+
 	public double getLon() {
 		return _lon;
 	}
