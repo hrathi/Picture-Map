855444e7b2ee15d6076f0ed73dbe4125d0c61c60
Harish Rathi <hrathi@rim.com>
2012-07-08 10:55:15 -0400

    Formatting

--- myapps/PicturesMapScreen.java
+++ myapps/PicturesMapScreen.java
@@ -28,63 +28,65 @@ class PicturesMapScreen extends FullScreen implements FieldChangeListener {
 	Screen _progressScreen;
 
 	public PicturesMapScreen() {
-		super(Screen.DEFAULT_CLOSE | Screen.DEFAULT_MENU | Manager.NO_VERTICAL_SCROLL) ;
+		super(Screen.DEFAULT_CLOSE | Screen.DEFAULT_MENU
+				| Manager.NO_VERTICAL_SCROLL);
 		_newActions = new ImageMapAction();
 		_geoImageVector = new SimpleSortingVector();
-		//_geoImageVector.setSortComparator(new MappableComparator());
-		
+		// _geoImageVector.setSortComparator(new MappableComparator());
+
 		_mapField = new ClusteredMapField();
 		_mapField.addChangeListener(this);
 		_mapField.setAutoUpdate(true);
-		_mapField.setAction(_newActions);		
-		_mapField.getAction().disableOperationMode( MapConstants.MODE_SHARED_FOCUS );
+		_mapField.setAction(_newActions);
+		_mapField.getAction().disableOperationMode(
+				MapConstants.MODE_SHARED_FOCUS);
 		_mapField.setStyle();
-		
+
 		// Activate pinch gesturing
 		if (Touchscreen.isSupported()) {
 			InputSettings is = TouchscreenSettings.createEmptySet();
 			is.set(TouchscreenSettings.DETECT_PINCH, 1);
 			this.addInputSettings(is);
 		}
-		
+
 		add(_mapField);
-		
-		UiApplication.getUiApplication().invokeLater(new Runnable() {			
+
+		UiApplication.getUiApplication().invokeLater(new Runnable() {
 			public void run() {
-				UiApplication.getUiApplication().pushScreen(new ProgressIndicatorScreen(PicturesMapScreen.this));
+				UiApplication.getUiApplication().pushScreen(
+						new ProgressIndicatorScreen(PicturesMapScreen.this));
 			}
 		});
 	}
-	
+
 	public void setImageVector(SimpleSortingVector vector) {
 		_geoImageVector = vector;
 		update();
 	}
-	
+
 	boolean superClusterDown = false;
 	ClusterMarkers _upMarker = null;
-	
+
 	public void fieldChanged(Field field, int actionId) {
-		switch (actionId)	 {
-		case MapAction.ACTION_FOCUSED_CHANGE:
-		{
-			if( field == _mapField ) {
+		switch (actionId) {
+		case MapAction.ACTION_FOCUSED_CHANGE: {
+			if (field == _mapField) {
 				Mappable m = _mapField.getFocusedMappable();
-				if( m instanceof ClusterMarkers ) {
-					_upMarker = (ClusterMarkers)m;
-					if( !_upMarker.isSuperCluster() ) {
-			            String uri = _upMarker.getIconUri();
-			            _upMarker.setIconUri("res://img/green" + uri.substring(uri.lastIndexOf('/')));
-			            _upMarker.update();
-		            }
-		            
-		            if( superClusterDown ) {
-	    				superClusterDown = false;
-	    				UiApplication.getUiApplication().pushScreen(new PictureViewerScreen(_upMarker._markers));
-	    			}
-		            else if( _upMarker.isSuperCluster() ) {
-		            	superClusterDown = true;
-		            }	
+				if (m instanceof ClusterMarkers) {
+					_upMarker = (ClusterMarkers) m;
+					if (!_upMarker.isSuperCluster()) {
+						String uri = _upMarker.getIconUri();
+						_upMarker.setIconUri("res://img/green" + uri.substring(uri.lastIndexOf('/')));
+						_upMarker.update();
+					}
+
+					if (superClusterDown) {
+						superClusterDown = false;
+						UiApplication.getUiApplication().pushScreen(
+								new PictureViewerScreen(_upMarker._markers));
+					} else if (_upMarker.isSuperCluster()) {
+						superClusterDown = true;
+					}
 				}
 			}
 			break;
@@ -94,81 +96,79 @@ class PicturesMapScreen extends FullScreen implements FieldChangeListener {
 			break;
 		}
 	}
-	
-	
+
 	protected final boolean touchEvent(TouchEvent message) {
-		if( message.getEvent() == TouchEvent.UP ) {
-			if( processUnclick(true) ) 
+		if (message.getEvent() == TouchEvent.UP) {
+			if (processUnclick(true))
 				return true;
 		}
-		
+
 		return super.touchEvent(message);
 	}
-	
+
 	protected boolean navigationUnclick(int status, int time) {
 		processUnclick(true);
 		return true;
 	}
-	
-	
-	
+
 	private boolean processUnclick(final boolean invokeScreen) {
-		if( _upMarker != null && !_upMarker.isSuperCluster() ) {
+		if (_upMarker != null && !_upMarker.isSuperCluster()) {
 			UiApplication.getUiApplication().invokeLater(new Runnable() {
-				
+
 				public void run() {
-					if( _upMarker != null ) {
+					if (_upMarker != null) {
 						String uri = _upMarker.getIconUri();
-			            _upMarker.setIconUri("res://img/red" + uri.substring(uri.lastIndexOf('/')));
-			            _upMarker.update();
-			            _mapField.update();
-			            UiApplication.getUiApplication().pushScreen(new PictureViewerScreen(_upMarker._markers));
-			            _upMarker = null;
+						_upMarker.setIconUri("res://img/red"
+								+ uri.substring(uri.lastIndexOf('/')));
+						_upMarker.update();
+						_mapField.update();
+						UiApplication.getUiApplication().pushScreen(
+								new PictureViewerScreen(_upMarker._markers));
+						_upMarker = null;
 					}
 				}
 			}, 1500, false);
-			
+
 			return true;
 		}
 		return false;
 	}
-		
-	
-	public static Bitmap getScaledBitmapImage(String imagePath, int scaleFactor, int width, int height) {
-		FileConnection connection=null;
-        byte[] byteArray=null;
-        Bitmap bitmap=null;
-        try
-        {
-            connection=(FileConnection)Connector.open(imagePath);
-            if(connection.exists())
-            {
-                byteArray=new byte[(int)connection.fileSize()];
-                InputStream inputStream=connection.openInputStream();
-                inputStream.read(byteArray);
-                inputStream.close();
-                bitmap = new Bitmap(width, height);
-                Bitmap b = Bitmap.createBitmapFromBytes(byteArray, 0, -1, scaleFactor);
-                b.scaleInto(bitmap, Bitmap.FILTER_BILINEAR, Bitmap.SCALE_TO_FILL);
-            }
-            connection.close();
-        }
-        catch(Exception e) {
-            System.out.println("Exception "+e.toString());
-        }
-        return bitmap;
-		
+
+	public static Bitmap getScaledBitmapImage(String imagePath,
+			int scaleFactor, int width, int height) {
+		FileConnection connection = null;
+		byte[] byteArray = null;
+		Bitmap bitmap = null;
+		try {
+			connection = (FileConnection) Connector.open(imagePath);
+			if (connection.exists()) {
+				byteArray = new byte[(int) connection.fileSize()];
+				InputStream inputStream = connection.openInputStream();
+				inputStream.read(byteArray);
+				inputStream.close();
+				bitmap = new Bitmap(width, height);
+				Bitmap b = Bitmap.createBitmapFromBytes(byteArray, 0, -1,
+						scaleFactor);
+				b.scaleInto(bitmap, Bitmap.FILTER_BILINEAR,
+						Bitmap.SCALE_TO_FILL);
+			}
+			connection.close();
+		} catch (Exception e) {
+			System.out.println("Exception " + e.toString());
+		}
+		return bitmap;
+
 	}
 
-   private void update() {
-	   _mapField.resetPoints();
-	   
-	   int mapPoints = _geoImageVector.size();
-	   for (int i = 0; i < mapPoints; i++) {
-		   GeoImage a = (GeoImage)_geoImageVector.elementAt(i);
-		   _mapField.updateGsFolder(a);
-	   }	
-	   
-	   _mapField.updateClusterCounters();
-    }
+	private void update() {
+		_mapField.resetPoints();
+
+		int mapPoints = _geoImageVector.size();
+		for (int i = 0; i < mapPoints; i++) {
+			GeoImage a = (GeoImage) _geoImageVector.elementAt(i);
+			_mapField.updateGsFolder(a);
+		}
+
+		_mapField.updateClusterCounters();
+	}
 }
